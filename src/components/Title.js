0eb522449a9a05c7a95c4f7ce0e875d2a54a1d7e
MridulTailor <mtailor729@gmail.com>
2023-03-13 01:34:18 +0530

    w1-d2

---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Title = () => {
+  return <h1>Day 2 Task</h1>;
+};
+
+export default Title;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Title from "../components/Title";
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Title />
+      </div>
+    );
+  }
+}
+
+export default Home;
