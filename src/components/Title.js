96c1c50295154950afeab4c0f07096b3336d8510
MridulTailor <mtailor729@gmail.com>
2023-03-13 10:48:56 +0530

    Day3

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Title = (props) => {
   return (
-    <h1>
-      My name is {props.name} {props.surName}
-    </h1>
+    <div>
+      <h1>
+        {/* My name is {props.name} {props.surName} */}
+        {props.info.title}
+      </h1>
+      <h1>{props.info.description}</h1>
+    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import Title from "../components/Title";
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Welcome Visitor",
+      description: "This is the description of the website",
+    };
+  }
+  changeTitle() {
+    this.setState({
+      title: "Welcome Mridul",
+    });
+  }
   render() {
     return (
       <div>
         <h1>Today is my {this.props.exam} exam.</h1>
-        <Title name="Mridul" surName="Tailor" />
-        <Title name="mriduL" surName="tailoR" />
-        <Title name="mRidul" surName="tAilor" />
+        <Title info={this.state} />
+        {/* <Title name="mriduL" surName="tailoR" />
+        <Title name="mRidul" surName="tAilor" /> */}
+        <button onClick={() => this.changeTitle()}>Change Text</button>
       </div>
     );
   }
